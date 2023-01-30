0bdcc66d0ee13a5e495f525b1f7fb28407cb8335
Dmitry Ostapchuk <95207376+WispHes@users.noreply.github.com>
2023-01-30 18:10:56 +0300

    Update E_Two_chips_2.py

--- python/0_sprint/E_Two_chips_2.py
+++ python/0_sprint/E_Two_chips_2.py
@@ -22,40 +22,25 @@
 Если таких пар не существует, то вывести «None».
 
 
+def two_sum(items, true_item):
+    items_set = set()
 
-from typing import List, Optional, Tuple
-
-
-def two_sum(arr: List[int], target_sum: int) -> Optional[Tuple[int, int]]:
-    arr.sort()
-
-    left = 0
-    right = len(arr) - 1
-    while left < right:
-        current_sum = arr[left] + arr[right]
-        if current_sum == target_sum:
-            return arr[left], arr[right]
-        if current_sum < target_sum:
-            left += 1
+    for a in items:
+        y = true_item - a
+        if y in items_set:
+            return a, y
         else:
-            right -= 1
-    return None
+            items_set.add(a)
+
+    return 'None',
 
 
-def read_input() -> Tuple[List[int], int]:
-    n = int(input())
-    arr = list(map(int, input().strip().split()))
-    target_sum = int(input())
-    return arr, target_sum
+if __name__ == '__main__':
+    _ = int(input())
+    print(
+        *two_sum(
+            [int(item) for item in input().split()],
+            int(input())
+        )
+    )
 
-
-def print_result(result: Optional[Tuple[int, int]]) -> None:
-    if result is None:
-        print(None)
-    else:
-        print(" ".join(map(str, result)))
-
-
-arr, target_sum = read_input()
-
-print_result(two_sum(arr, target_sum))

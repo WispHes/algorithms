0e96079a05924e8bd86ef4322861ff4737b5fbb3
Dmitry Ostapchuk <95207376+WispHes@users.noreply.github.com>
2023-01-30 18:09:43 +0300

    Update C_Moving_Average.py

--- python/0_sprint/C_Moving_Average.py
+++ python/0_sprint/C_Moving_Average.py
@@ -24,26 +24,18 @@
 
 
 
-from typing import List, Tuple
-
-
-def moving_average(chars: List[int], win: int) -> List[float]:
+def moving_average(timeseries, k):
     result = []
-    value = sum(chars[0:win])
-    result.append(value/win)
-    for i in range(0, len(chars)-win):
-        value -= chars[i]
-        value += chars[i+win]
-        result.append(value/win)
+    current_sum = sum(timeseries[0:k])
+    result.append(current_sum/k)
+    for index in range(0, len(timeseries)-k):
+        current_sum -= timeseries[index]
+        current_sum += timeseries[index+k]
+        result.append(current_sum/k)
     return result
 
 
-def read_input() -> Tuple[List[int], int]:
-    n = int(input())
-    chars = list(map(int, input().strip().split()))
-    win = int(input())
-    return chars, win
-
-
-chars, win = read_input()
-print(' '.join(map(str, moving_average(chars, win))))
+if __name__ == '__main__':
+    _ = int(input())
+    items = [int(item) for item in input().split()]
+    print(*moving_average(items, int(input())))
